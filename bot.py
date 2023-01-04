5ca4f207b3bc2b54cc9105af6543a0bd3b9179de
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-01-04 20:10:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ mergeApp = MergeBot(
     bot_token=Config.BOT_TOKEN,
     workers=300,
     plugins=dict(root="plugins"),
-    app_version="5.0+yash-mergebot",
+    app_version="5.0+mergebot",
 )
 
 Popen("gunicorn app:app", shell=True)
